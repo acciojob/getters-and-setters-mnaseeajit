944bea6971972f103ccd4076e0e732895719271d
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-17 11:12:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //complete this code
 class Person {
-	constructor(name , age){
+	constructor(name, age){
 		this._name = name;
 		this._age = age;
 	}
-	get Name(){
-		return `my name is ${this._name}`;
+	get name(){
+		return this._name;
 	}
-	set Age(age){
+	set age(age){
 		this._age = age;
 	}
 	get age(){
-		return this.age;
+		return this._age;
 	}
 }
 
